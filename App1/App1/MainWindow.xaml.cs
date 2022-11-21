1cacf8cc06909bc4beb7035aa86b07ab9ce2b817
2130666 <nathan.lafreniere.01@edu.cegeptr.qc.ca>
2022-11-21 13:20:19 -0500

    modif nav

--- App1/App1/MainWindow.xaml.cs
+++ App1/App1/MainWindow.xaml.cs
@@ -28,9 +28,6 @@ namespace App1
             this.InitializeComponent();
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
-        }
+     
     }
 }
