ba1485744615d720241753d6415c7236877aa683
ABlaisOuellette <alexandre.blais.ouellette.01@edu.cegeptr.qc.ca>
2022-11-21 14:36:29 -0500

    push singleton

---
+++ App1/App1/GestionBD.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace App1
+{
+    internal class GestionBD
+    {
+        MySqlConnection con;
+        static GestionBD gestionBD = null;
+         
+
+
+        public GestionBD()
+        {
+            this.con = new MySqlConnection("Server=cours.cegep3r.info;Database=a2022_420326_eq5;Uid=2130666;Pwd=2130666");
+        }
+
+        public static GestionBD getInstance()
+        {
+            if (gestionBD == null) 
+                gestionBD = new GestionBD();
+
+            return gestionBD;
+        }
+
+        public ObservableCollection<Trajet> getTrajet()
+        {
+
+        }
+    }
+}
